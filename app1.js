aa4db767732053024f17425b13b076dac0b35fe8
nura <nurgul.bekisheva.kk@gmail.com>
2022-07-23 12:02:28 +0600

    Add js files

---
+++ app1.js
@@ -0,0 +1,12 @@
+let arr = ["Иван", "Иванов", "отдел разработки", "программист", 2000];
+
+
+// DESTRUCTURING
+let [name, surname, department, position, salary] = arr;
+
+console.log(name);
+console.log(surname);
+console.log(department);
+console.log(position);
+console.log(salary);
+
